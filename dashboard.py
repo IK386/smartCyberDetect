cfab320fd499ca245ce27bf2f40c8710b79aebc6
(ik_ys)CSD <ikramlaidi713@gmail.com>
2026-04-11 04:12:23 -0700

    Refactor dashboard.py for improved readability

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
